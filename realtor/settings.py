ebcf9c3cf9dd4d050b160945cd709dc9e179536e
HaashirBinShakeel <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-02-24 19:34:19 +0500

    working frontend

--- realtor/settings.py
+++ realtor/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 STATIC_ROOT=os.path.join(BASE_DIR ,'static')
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / "realtor/static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
