35f65e4bc50b36f7a51ee2614e1f6cedf838ac2b
HaashirBinShakeel <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-03-08 00:14:25 +0500

    checks for register

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,34 @@
 from django.contrib import messages
 from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        messages.error(request,'Testing Our messages')
-        return redirect('register')
+        #get user values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']        
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']        
+        password2 = request.POST['password2']
+
+    	#checking password match
+
+        if password == password2:
+            #Check if username exists
+            if User.objects.filter(username = username).exists():
+                messages.error(request,'Username already exists')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request,'Email is already in use ')
+                else:
+                    #looks good
+                    return
+        else:
+            messages.error(request, 'Passwords donot match')
+            return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
